ff3c5c35865823abd85b0859e4f50350c9febaa7
Muhammad Usman <musmangill68@gmail.com>
2026-01-19 17:15:26 +0500

    Completed

--- lib/utils/api_key.dart
+++ lib/utils/api_key.dart
@@ -2,13 +2,5 @@ class ApiKey {
   static final String pexelImageAPIURL = 'https://api.pexels.com/v1/curated';
   static final String pexelSearchAPIURL =
       'https://api.pexels.com/v1/search?query=women';
-  static final String pexelAPIKey =
-      "leb3kBATgHS5aZOnXIwgq4u4WmQ3qp9Os3EmF8WGRQ992tlPGyJoaKqV";
+  static final String pexelAPIKey = "USE YOU OWN SPECIAL PEXEL API KEY HERE";
 }
-
-
-// fetch("https://api.pexels.com/v1/search?query=people", {
-//   headers: {
-//     Authorization: 'YOUR_API_KEY'
-//   })
-
